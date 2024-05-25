875da4581482b37c8f408f6a1dd7124cf25cb0b3
samya-roy <samyaroy999@gmail.com>
2024-05-25 23:11:41 +0600

    delete add cart

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -24,9 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
+    
 
     const collectionBistro = client.db('bostroDB').collection('menu')
     const collectionCarts = client.db('bostroDB').collection('carts')
@@ -47,6 +45,13 @@ async function run() {
       const quary = {email: email}
       const result = await collectionCarts.find(quary).toArray()
       res.send(result)
+    })
+
+    app.delete('/carts/:id', async (req, res)=> {
+      const id = req.params.id
+      const quary = {_id: new ObjectId(id)}
+      const result = await collectionCarts.deleteOne(quary)
+      res.send(result) 
     })
 
     await client.db("admin").command({ ping: 1 });
