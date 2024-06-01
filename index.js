ab017b39b6aed3f47a58090da62d7596957e9458
samya-roy <samyaroy999@gmail.com>
2024-06-01 10:11:39 +0600

    manage item delete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
@@ -25,23 +26,124 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
-
     const collectionBistro = client.db('bostroDB').collection('menu')
     const collectionCarts = client.db('bostroDB').collection('carts')
     const collectionUsers = client.db('bostroDB').collection('users')
+
+
+    // jwt releted api 
+    app.post("/jwt", async (req, res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
+    })
+
 
     // auth releted api
 
     app.post('/user', async (req, res) => {
       const body = req.body
+      const quary = { email: body.email }
+      const existingUser = await collectionUsers.findOne(quary)
+
+      if (existingUser) {
+        return res.send({ massages: 'user already exist', insertedID: null })
+      }
+
       const result = await collectionUsers.insertOne(body)
       res.send(result)
+
     })
+
+    // midelwera
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'forbidden access' })
+      }
+      // next()
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ massages: 'forbidden access' })
+        }
+        req.decoded = decoded
+        next()
+      })
+    }
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email
+      const quary = { email: email }
+      const user = await collectionUsers.findOne(quary)
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ massages: 'forbidden access' })
+      }
+      next()
+    }
+    // midelwera
+
+
+    app.get('/user', verifyToken, verifyAdmin, async (req, res) => {
+      // console.log(req.headers);
+      const result = await collectionUsers.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/user/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ massages: 'unAuthorized token' })
+      }
+      const quary = { email: email }
+      const result = await collectionUsers.findOne(quary)
+      let admin = false
+      if (result) {
+        admin = result?.role === 'admin'
+      }
+      res.send({ admin })
+    })
+
+
+    app.delete('/user/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id
+      const quary = { _id: new ObjectId(id) }
+      const result = await collectionUsers.deleteOne(quary)
+      res.send(result)
+    })
+
+    app.patch('/user/admin/:id', verifyToken, verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) }
+        const updataDoc = {
+          $set: {
+            role: "admin"
+          }
+        }
+        const result = await collectionUsers.updateOne(filter, updataDoc)
+        res.send(result)
+      })
+
 
     // data releted api
 
     app.get('/menu', async (req, res) => {
       const result = await collectionBistro.find().toArray()
+      res.send(result)
+    })
+
+    app.post('/menu', async (req, res) => {
+      const item = req.body
+      const result = await collectionBistro.insertOne(item)
+      res.send(result)
+    })
+
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id
+      const quary = { _id: new ObjectId(id) }
+      const result = await collectionBistro.deleteOne(quary);
       res.send(result)
     })
 
@@ -61,11 +163,6 @@ async function run() {
     app.delete('/carts/:id', async (req, res) => {
       const id = req.params.id
       const user = { _id: new ObjectId(id) }
-      const quary = {email : user.email}
-      const existingUser = await collectionUsers.findOne(quary)
-      if (existingUser) {
-        return res.send({massages : 'user already exist', insertedID: null})
-      }
       const result = await collectionCarts.deleteOne(user)
       res.send(result)
     })
